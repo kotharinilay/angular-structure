0e89e666110b06fcab4a3fde8369f7c9f2b0e61b
Nilay Kothari <kothari_nilay@yahoo.co.in>
2017-12-26 23:01:37 +0530

    interface change

--- src/app/shared/interfaces/http-interface.ts
+++ src/app/shared/interfaces/http-interface.ts
@@ -1,14 +1,14 @@
 import { HttpHeaders } from '@angular/common/http/src/headers';
 import { HttpParams } from '@angular/common/http/src/params';
 
-export class IHttpOptions {
-    public observe?: string;
-    public reportProgress?: boolean;
-    public responseType?: string;
-    public withCredentials?: boolean;
+export interface IHttpOptions {
+    observe?: string;
+    reportProgress?: boolean;
+    responseType?: string;
+    withCredentials?: boolean;
 }
 
-export class IRequestOptions extends IHttpOptions {
-    public headers?: HttpHeaders;
-    public params?: HttpParams;
+export interface IRequestOptions extends IHttpOptions {
+    headers?: HttpHeaders;
+    params?: HttpParams;
 }
